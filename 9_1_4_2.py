bcca82dd34bc1eb30afe5b01a0e5d2584d3d81c6
zkzk5214 <59314355+zkzk5214@users.noreply.github.com>
2021-01-18 21:38:09 +0800

    1-18-2021

---
+++ 9_1_4_2.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+rect = plt.Rectangle((0.2, 0.75), 0.4, 0.15, color='k', alpha=0.3)
+circ = plt.Circle((0.7, 0.2), 0.15, color='b', alpha=0.3)
+pgon = plt.Polygon([[0.15, 0.15], [0.35, 0.4], [0.2, 0.6]],
+                   color='g', alpha=0.5)
+
+ax.add_patch(rect)
+ax.add_patch(circ)
+ax.add_patch(pgon)
+plt.show()
